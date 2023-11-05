f5accc1ea5cc64e3ec509081a9f12c542c9b30ef
Devashish Singh Parmar <parmardevashish132@gmail.com>
2023-11-05 17:15:58 +0530

    forEach DSA.

--- 7kyu/forEach.js
+++ 7kyu/forEach.js
@@ -16,3 +16,28 @@ var number=function(array){
     return newArr
     }
   
+
+
+
+    // Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+// For Example:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// Note: You will always be given a non-empty list containing positive values.
+
+// ENJOY CODING :)
+
+function sumOfMinimums(arr) {
+     // your code here
+     let sumOfMin = 0;
+     arr.forEach(element=>{
+       sumOfMin += Math.min(...element);
+     })
+     return sumOfMin
+   }
