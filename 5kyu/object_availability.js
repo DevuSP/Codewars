338250cf5c7056146dd99cc03578c424f27742c9
Devashish Singh Parmar <parmardevashish132@gmail.com>
2023-12-31 20:06:53 +0530

    DSA

---
+++ 5kyu/object_availability.js
@@ -0,0 +1,29 @@
+// Cake Recipe vs Availability.
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+// Examples:
+
+// // must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+// // must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+
+function cakes(recipe, available) {
+    let quantity = 100;
+    for (ingredients in recipe) {
+      let amountCouldBake = Math.floor(available[ingredients] / recipe[ingredients]);
+      if (!amountCouldBake) {
+        return 0
+      } else if (amountCouldBake < quantity) {
+        quantity = amountCouldBake;
+      } 
+    }
+    return quantity;
+  }
+  
+  
+  // const res = cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 });
+  const res = cakes({ crumbles: 5, butter: 4, pears: 4, flour: 0 }, { flour: 0, eggs: 11, pears: 13, butter: 16, crumbles: 13 });
+  console.log(res);
